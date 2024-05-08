20b5cc07c7c5ff5b98120e762e00e12ceff0dc42
night <night.slumber@outlook.com>
2024-05-08 21:09:57 +0800

    update in 2024-5-8

---
+++ 06-src-scoped样式/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1 class="title">fjsadjfs</h1>
+        <School/>
+        <Student/>
+    </div>
+</template>
+
+<script>
+    import Student from './components/Student'
+    import School from './components/School'
+
+
+    export default {
+        name: 'App',
+        components: {
+            School,
+            Student,
+        }
+    }
+</script>
+
+<style scoped>
+    .title {
+        color: red;
+    }
+</style>
